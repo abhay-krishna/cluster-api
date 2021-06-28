7d9db58a29fdbb6773309b869c27e82b55caccde
Rajashree Mandaogane <mandaor@amazon.com>
2021-06-28 13:44:50 -0700

    Add unstacked etcd support
    Unstacked etcd: API and config changes
    
    Unstacked etcd: Changes in CAPI core controllers
    
    This commit adds the following changes in the cluster controller:
    * A change in reconcileControlPlane to check if the cluster is using managed
    external etcd, and if etcd is not ready then pause the control plane provisioning.
    * A new phase in cluster controller's phases for reconciling the etcd cluster. If the
    etcd cluster is ready then this phase will resume control plane provisioning
    
    This commit also adds the following change in the machine controller:
    * The machine controller upon creation of the first etcd machine will save its IP.
    
    Unstacked etcd: Changes in KCP controller
    
    The KubeadmControlPlane controller will get the external etcd endpoints
    from the object referenced by cluster.spec.managedExternalEtcdRef.
    The validating webhook will allow the external etcd endpoints to be updated.
    
    Unstacked etcd: Change in docker infra provider
    
    Docker is the only infrastructure provider that performs a kubectl patch on the k8s
    node corresponding to a Machine. This needs to be skipped for etcd machines since they
    are not registered as nodes and do not run kubelet.
    
    Unstacked etcd: Ignore nodeRef check for etcd machines during clusterctl move
    
    Clusterctl before beginning the move checks if all CAPI objects are
    ready and provisioned. One of these checks is for Machine.Status.NodeRef field.
    This check needs to be skipped for etcd machines since they are not registered
    as Kubernetes nodes so they don't have a corresponding Node.
    
    Delete managed external etcd cluster last
    
    Update KCP controller to use renamed etcd endpoints field
    
    The field 'endpoint' on EtcdadmCluster's status has been renamed
    to 'endpoints'. This commit updates the KCP controller to use the
    renamed field
    
    cr https://code.amazon.com/reviews/CR-54310674
    
    Add external etcd api changes to v1beta1 capi CRDs
    
    Fix etcdMachine to cluster reconciler so it listens on Machine events
    
    cr: https://code.amazon.com/reviews/CR-56463335
    
    Add external etcd api to v1alpha4
    
    Fix watch on machine object for etcdMachine to cluster mapper
    
    While cherry-picking commits from 0.3.19 branch the watch got modified
    by mistake. This commit fixes it by changing it back to watching Machine
    objects.
    
    Retain update permission on etcdadmcluster for KCP controller
    
    KCP controller updates the etcdadmcluster object once KCP upgrade is completed.
    It needs update permission on etcdadmcluster object for this.
    We previously had added this permission, it got dropped while rebasing
    commits on the new 1.0.1 branch. This commit adds back the permission.

--- controlplane/kubeadm/api/v1beta1/kubeadm_control_plane_webhook.go
+++ controlplane/kubeadm/api/v1beta1/kubeadm_control_plane_webhook.go
@@ -145,6 +145,7 @@ func (in *KubeadmControlPlane) ValidateUpdate(old runtime.Object) (admission.War
 		{spec, kubeadmConfigSpec, clusterConfiguration, "etcd", "local", "imageTag"},
 		{spec, kubeadmConfigSpec, clusterConfiguration, "etcd", "local", "extraArgs"},
 		{spec, kubeadmConfigSpec, clusterConfiguration, "etcd", "local", "extraArgs", "*"},
+		{spec, kubeadmConfigSpec, clusterConfiguration, "etcd", "external", "endpoints"},
 		{spec, kubeadmConfigSpec, clusterConfiguration, "dns", "imageRepository"},
 		{spec, kubeadmConfigSpec, clusterConfiguration, "dns", "imageTag"},
 		{spec, kubeadmConfigSpec, clusterConfiguration, "imageRepository"},

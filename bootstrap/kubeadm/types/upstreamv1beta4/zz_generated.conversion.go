1b3c50041c958e5bbe0466ce076ce1b6a677a6a6
Aravind Ramalingam <ramaliar@amazon.com>
2024-01-22 12:47:26 -0800

    Adding support for multiple registry mirrors in bottlerocket

--- bootstrap/kubeadm/types/upstreamv1beta4/zz_generated.conversion.go
+++ bootstrap/kubeadm/types/upstreamv1beta4/zz_generated.conversion.go
@@ -448,6 +448,7 @@ func autoConvert_v1beta1_ClusterConfiguration_To_upstreamv1beta4_ClusterConfigur
 	// WARNING: in.BottlerocketHostContainers requires manual conversion: does not exist in peer-type
 	// WARNING: in.BottlerocketCustomBootstrapContainers requires manual conversion: does not exist in peer-type
 	// WARNING: in.Bottlerocket requires manual conversion: does not exist in peer-type
+	// WARNING: in.CertBundles requires manual conversion: does not exist in peer-type
 	return nil
 }
 
@@ -762,6 +763,7 @@ func autoConvert_v1beta1_JoinConfiguration_To_upstreamv1beta4_JoinConfiguration(
 	// WARNING: in.BottlerocketCustomHostContainers requires manual conversion: does not exist in peer-type
 	// WARNING: in.BottlerocketCustomBootstrapContainers requires manual conversion: does not exist in peer-type
 	// WARNING: in.Bottlerocket requires manual conversion: does not exist in peer-type
+	// WARNING: in.CertBundles requires manual conversion: does not exist in peer-type
 	return nil
 }
 
